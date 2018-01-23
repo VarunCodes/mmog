10e393c26abf9e6d11acd3477507b50c046df2c7
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-23 10:04:55 +0000

    changed room.coffee to js

---
+++ app/assets/javascripts/channels/rooms.js
@@ -0,0 +1,39 @@
+jQuery(document).on('turbolinks:load', function() {
+  var App_global_chat, messages, messages_to_bottom;
+  messages = $('#messages');
+  if ($('#messages').length > 0) {
+    messages_to_bottom = function() {
+      return messages.scrollTop(messages.prop("scrollHeight"));
+    };
+    messages_to_bottom();
+    App_global_chat = App.cable.subscriptions.create({
+      channel: "ChatRoomsChannel",
+      chat_room_id: messages.data('chat-room-id')
+    }, {
+      connected: function() {},
+      disconnected: function() {},
+      received: function(data) {
+        console.log('message received');
+        console.log(data);
+        return messages.append(data['message']);
+      },
+      send_message: function(message, chat_room_id) {
+        return this.perform('send_message', {
+          message: message,
+          chat_room_id: chat_room_id
+        });
+      }
+    });
+    return $('#new_message').submit(function(e) {
+      var $this, textarea;
+      $this = $(this);
+      textarea = $this.find('#message_body');
+      if ($.trim(textarea.val()).length > 1) {
+        App_global_chat.send_message(textarea.val(), messages.data('chat-room-id'));
+        textarea.val('');
+        e.preventDefault();
+        return false;
+      }
+    });
+  }
+});
