656bccd6fdaef73ceb6f7f3e9bcda305cdfe9e17
Keran <kerbraich@gmail.com>
2018-01-25 16:41:45 +0000

    remove hashed out code that is not being used

--- playing.rb
+++ playing.rb
@@ -12,17 +12,3 @@ cxt.load("#{filepath}/app/assets/javascripts/game/game.js")
 cxt.eval("game = new Game('mychannel')")
 
 p cxt[:game]
-
-
-# a = ExecJS.compile "function a() {
-# this.value = 0;
-# }
-#
-# a.prototype.starttimer = function() {
-#   setInterval(function() { this.value })
-# }
-# something = new a()
-# "
-#
-# # p a.eval("")
-# p a.eval("something.starttimer()")
